1174439da0def80f624da93f96322204a1db5726
John Brewer <jbrewer@jera.com>
2020-09-28 15:54:33 -0700

    Add, hook up AuCDemoViewController

---
+++ iOS/Objective-C/SampleObjC/AuCDemoViewController.h
@@ -0,0 +1,17 @@
+//
+//  AuCDemoViewController.h
+//  SampleObjC
+//
+//  Created by John Brewer on 9/28/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AuCDemoViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS/Objective-C/SampleObjC/AuCDemoViewController.m
@@ -0,0 +1,32 @@
+//
+//  AuCDemoViewController.m
+//  SampleObjC
+//
+//  Created by John Brewer on 9/28/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+#import "AuCDemoViewController.h"
+
+@interface AuCDemoViewController ()
+
+@end
+
+@implementation AuCDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
